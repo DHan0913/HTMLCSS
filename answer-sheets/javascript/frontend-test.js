b623149a66a0e349640076f2c44f89a8f2806535
DHan0913 <eogml01023@gmail.com>
2024-07-03 16:50:14 +0900

    .

---
+++ answer-sheets/javascript/frontend-test.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    document.getElementById('btnHello').addEventListener('click', function() {
+        var message = '안녕하세요!';
+        alert(message);
+    });
+});
+
